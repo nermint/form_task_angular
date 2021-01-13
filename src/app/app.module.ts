18fa3b930a2a2ab92dccc4773b1cbf009eca1a72
nermint <talibovanermin1999@gmail.com>
2021-01-13 18:22:06 +0400

    folder_structure_2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BannerComponent } from './core/components/banner/banner.component';
+import { FormComponent } from './core/pages/form/form.component';
+import { SuccessComponent } from './core/pages/success/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    FormComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
