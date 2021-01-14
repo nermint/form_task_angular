f556be73c1c3af47488e44bffae341e6f2af6462
nermint <talibovanermin1999@gmail.com>
2021-01-14 15:25:44 +0400

    adding_sections_btn_click

--- src/app/core/pages/form/form.component.ts
+++ src/app/core/pages/form/form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormGroup,FormControl,FormBuilder, Validators } from '@angular/forms';
+import { FormGroup,FormBuilder, Validators,FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -10,19 +10,8 @@ import { FormGroup,FormControl,FormBuilder, Validators } from '@angular/forms';
 export class FormComponent implements OnInit {
 
   formGroup:FormGroup;
+  fileName = '';
   initForm(){
-    // this.formGroup = this.formBuilder.group({
-    //   name: new FormControl('',[ Validators.required]),
-    //   surname: new FormControl('', [Validators.required]),
-    //   fathername: new FormControl('', [Validators.required]),
-    //   birthdate: new FormControl('',[Validators.required]),
-    //   familyAddress: new FormControl('', [Validators.required]),
-    //   regionId: new FormControl(0,[Validators.required]),
-    //   contactInfo: new FormControl('',[Validators.required]),
-    //   fin: new FormControl('',[Validators.required]),
-    //   identityPhotoId: new FormControl('',[Validators.required]),
-    // })
-
     this.formGroup = this.fb.group({
       name: ['', Validators.required],
       surname: ['', Validators.required],
@@ -32,24 +21,39 @@ export class FormComponent implements OnInit {
       regionId: ['', Validators.required],
       contactInfo: ['', Validators.required],
       fin: ['', Validators.required],
-      identityPhotoId: ['', Validators.required]
+      identityPhotoId: ['', Validators.required],
+      rewards: this.fb.array([this.initRewardsItem()])
     })
   }
 
-  fileName = '';
-
-  constructor(private fb: FormBuilder,) { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.initForm();
   }
 
+  initRewardsItem() {
+    return this.fb.group({
+      name: [''],
+      date:['']
+    });
+  }
+
+
+  get rewardsArr() {
+    return this.formGroup.get('rewards') as FormArray;
+  }
+
+  addNewRow() {
+    this.rewardsArr.push(this.initRewardsItem());
+  }
+
   onFileChange($event) {
-    let file = $event.target.files[0]; // <--- File Object for future use.
+    let file = $event.target.files[0];
     console.log(file);
-    this.formGroup.controls['identityPhotoId'].setValue(file ? file.name : ''); // <-- Set Value for Validation
+    this.formGroup.controls['identityPhotoId'].setValue(file ? file.name : '');
     this.fileName = file.name;
     console.log(this.formGroup.controls['identityPhotoId'].value);
-}
+ }
 
 }
--- src/app/core/pages/success/success.component.ts
+++ src/app/core/pages/success/success.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-success',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./success.component.css']
 })
 export class SuccessComponent implements OnInit {
+  public invoiceForm: FormGroup;
 
-  constructor() { }
+  constructor(private _fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.invoiceForm = this._fb.group({
+      itemRows: this._fb.array([this.initItemRows()])
+    });
+  }
+
+  get formArr() {
+    return this.invoiceForm.get('itemRows') as FormArray;
+  }
+
+  initItemRows() {
+    return this._fb.group({
+      itemname: ['']
+    });
+  }
+
+  addNewRow() {
+    this.formArr.push(this.initItemRows());
+  }
+
+  deleteRow(index: number) {
+    this.formArr.removeAt(index);
   }
 
 }
