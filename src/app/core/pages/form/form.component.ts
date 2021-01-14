022724554bdb38253d9ba430c01767d066974857
nermint <talibovanermin1999@gmail.com>
2021-01-14 23:58:23 +0400

    date_format_and_data_general

--- src/app/core/pages/form/form.component.ts
+++ src/app/core/pages/form/form.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { FormGroup,FormBuilder, Validators,FormArray } from '@angular/forms';
 
+import {formatDate} from '@angular/common';
+
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -13,15 +16,16 @@ export class FormComponent implements OnInit {
   fileName = '';
   initForm(){
     this.formGroup = this.fb.group({
-      name: ['', Validators.required],
-      surname: ['', Validators.required],
-      fathername: ['', Validators.required],
-      birthdate: ['', Validators.required],
-      familyAddress: ['', Validators.required],
-      regionId: ['', Validators.required],
-      contactInfo: ['', Validators.required],
-      fin: ['', Validators.required],
-      identityPhotoId: ['', Validators.required],
+      name: ['', [Validators.maxLength(50), Validators.minLength(2)]],
+      surname: ['', [Validators.maxLength(50), Validators.minLength(2)]],
+      fathername: ['', [Validators.maxLength(50), Validators.minLength(2)]],
+      birthdate: [''],
+      familyAddress: ['', [Validators.maxLength(250), Validators.minLength(2)]],
+      regionId: ['', [Validators.min(0)]],
+      dateOfMartyrdomOrVeteran: [''],
+      contactInfo: ['', [Validators.maxLength(250), Validators.minLength(1)]],
+      fin: ['', [Validators.maxLength(7), Validators.minLength(7)]],
+      identityPhotoId: [''],
       rewards: this.fb.array([this.initRewardsItem()]),
       children: this.fb.array([this.initChildrenItem()]),
       apartments: this.fb.array([this.initApartmentsItem()])
@@ -32,6 +36,9 @@ export class FormComponent implements OnInit {
 
   ngOnInit(): void {
     this.initForm();
+    console.log(this.photos[0]['controls'].photos.value);
+    console.log(this.name);
+    // this.dateFormat(this.birthdate);
   }
 
   initRewardsItem() {
@@ -78,6 +85,53 @@ export class FormComponent implements OnInit {
   get apartmentArr() {
     return this.formGroup.get('apartments') as FormArray;
   }
+  get photos(){
+    return (this.formGroup.get('apartments') as FormArray).controls;
+  }
+
+  // get form data
+  get name(){
+    return this.formGroup.get('name').value;
+  }
+  get surname(){
+    return this.formGroup.get('surname').value;
+  }
+  get fathername(){
+    return this.formGroup.get('fathername').value;
+  }
+  get birthdate(){
+    return this.formGroup.get('birthdate').value;
+  }
+  get familyAddress(){
+    return this.formGroup.get('familyAddress').value;
+  }
+  get regionId(){
+    return this.formGroup.get('regionId').value;
+  }
+  get dateOfMartyrdomOrVeteran(){
+    return this.formGroup.get('dateOfMartyrdomOrVeteran').value;
+  }
+  get contactInfo(){
+    return this.formGroup.get('contactInfo').value;
+  }
+  get fin(){
+    return this.formGroup.get('fin').value;
+  }
+  get identityPhotoId(){
+    return this.formGroup.get('identityPhotoId').value;
+  }
+
+
+  dateFormat(date){
+    const format = 'yyyy-MM-ddTHH:mm:ss.SSSSSSS';
+    const locale = 'en-US';
+    let formattedDate='';
+    if(date){
+      formattedDate = formatDate(date, format, locale);
+    }
+    console.log(formattedDate);
+    return formattedDate;
+  }
 
   addNewReward() {
     this.rewardsArr.push(this.initRewardsItem());
@@ -92,10 +146,31 @@ export class FormComponent implements OnInit {
 
   onFileChange($event) {
     let file = $event.target.files[0];
-    console.log(file);
     this.formGroup.controls['identityPhotoId'].setValue(file ? file.name : '');
-    this.fileName = file.name;
+    // this.fileName = file.name;
     console.log(this.formGroup.controls['identityPhotoId'].value);
+ }
+
+
+ getFormData(){
+    const data = {
+      name: this.name,
+      surname: this.surname,
+      fathername: this.fathername,
+      birthdate: this.dateFormat(this.birthdate),
+      familyAddress: this.familyAddress,
+      regionId: this.regionId,
+      dateOfMartyrdomOrVeteran: this.dateFormat(this.dateOfMartyrdomOrVeteran),
+      contactInfo: this.contactInfo,
+      fin: this.fin,
+      identityPhotoId: this.identityPhotoId,
+      rewards: this.rewardsArr.value,
+      children: this.childrenArr.value,
+      apartments: this.apartmentArr.value
+    }
+
+    console.log(data);
+  
  }
 
 }
