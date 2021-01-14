ef7030f766065d23a4ba7a054cebf3df0fc1340c
nermint <talibovanermin1999@gmail.com>
2021-01-14 16:35:22 +0400

    form_arrangement_2

--- src/app/core/pages/form/form.component.ts
+++ src/app/core/pages/form/form.component.ts
@@ -22,7 +22,9 @@ export class FormComponent implements OnInit {
       contactInfo: ['', Validators.required],
       fin: ['', Validators.required],
       identityPhotoId: ['', Validators.required],
-      rewards: this.fb.array([this.initRewardsItem()])
+      rewards: this.fb.array([this.initRewardsItem()]),
+      children: this.fb.array([this.initChildrenItem()]),
+      apartments: this.fb.array([this.initApartmentsItem()])
     })
   }
 
@@ -39,13 +41,46 @@ export class FormComponent implements OnInit {
     });
   }
 
+  initChildrenItem(){
+    return this.fb.group({
+      name:[''],
+      surname: [''],
+      fin: [''],
+      birthdate: [''],
+      gender: [0],
+      identityPhotoId: ['']
+    });
+  }
+
+  initApartmentsItem(){
+    return this.fb.group({
+      peopleCount:[0],
+      totalArea: [0],
+      roomCount: [0],
+      hasDocument: [false],
+      photos:this.fb.array([this.initApartmentPhoto()])
+    });
+  }
+  initApartmentPhoto(){
+    return this.fb.group({
+      photoId:['']
+    });
+  }
+
 
   get rewardsArr() {
     return this.formGroup.get('rewards') as FormArray;
   }
+  get childrenArr() {
+    return this.formGroup.get('children') as FormArray;
+  }
 
-  addNewRow() {
+  addNewReward() {
     this.rewardsArr.push(this.initRewardsItem());
+    console.log(this.rewardsArr);
+  }
+  addChild() {
+    this.childrenArr.push(this.initChildrenItem());
   }
 
   onFileChange($event) {
